47c3f46d8b086922daa8daf0e226789648aee011
aniketvw <aniketvw14@gmail.com>
2024-12-03 13:14:44 +0530

    TheoryCommit

--- Eight/src/main/java/org/example/Main.java
+++ Eight/src/main/java/org/example/Main.java
@@ -1,7 +1,17 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.function.IntFunction;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
+    public static void main(String[] args) {
+
+
+
+    }
 
 }
